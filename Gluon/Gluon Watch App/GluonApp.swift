c85125db27662663849cee2a5d3685e1a87036a7
Dakota Kim <codingvampire@gmail.com>
2024-04-15 20:23:46 -0400

    add Gluon Apple Watch app

---
+++ Gluon/Gluon Watch App/GluonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GluonApp.swift
+//  Gluon Watch App
+//
+//  Created by Dakota Kim on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Gluon_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
